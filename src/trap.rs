57ec536b2d2f279e9401ac18daa05e718fba34cc
Stephen Marz <sgm@utk.edu>
2022-06-29 16:55:01 -0400

    Add comments

--- src/trap.rs
+++ src/trap.rs
@@ -4,11 +4,13 @@ pub fn rust_trap() {
     let interrupt = mcause >> 31 & 1 == 1;
 
     if interrupt {
+        // Interrupt (asynchronous)
         match mcause & 0xFF {
             11 => crate::imsic::imsic_handle(),
             _ => println!("Interrupt {}", mcause),
         }
     } else {
+        // Exception (synchronous)
         println!("Exception {} @ 0x{:08x}: 0x{:08x}", mcause, csr_read!("mepc"), csr_read!("mtval"));
         crate::abort();
     }
